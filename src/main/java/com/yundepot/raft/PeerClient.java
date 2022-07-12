38aecee87dbcb2d244596dbcf91a7e30d9f0879a
zhaiyanan <429276924@qq.com>
2022-07-12 14:52:55 +0800

    init

---
+++ src/main/java/com/yundepot/raft/PeerClient.java
@@ -0,0 +1,39 @@
+package com.yundepot.raft;
+
+import com.yundepot.oaa.config.GenericOption;
+import com.yundepot.raft.bean.*;
+import com.yundepot.raft.service.RaftService;
+import com.yundepot.rpc.RpcClient;
+
+/**
+ * 集群内节点之前通信
+ * @author zhaiyanan
+ * @date 2019/6/15 17:30
+ */
+public class PeerClient {
+    private RpcClient rpcClient;
+    private RaftService raftService;
+
+    public PeerClient(String host, int port) {
+        this.rpcClient = new RpcClient(host, port);
+        this.rpcClient.option(GenericOption.TCP_HEARTBEAT_SWITCH, true);
+        this.rpcClient.start();
+        raftService = this.rpcClient.create(RaftService.class);
+    }
+
+    public void shutdown() {
+        this.rpcClient.shutdown();
+    }
+
+    public VoteResponse requestVote(VoteRequest request) {
+        return raftService.requestVote(request);
+    }
+
+    public AppendLogResponse appendLog(AppendLogRequest request) {
+        return raftService.appendLog(request);
+    }
+
+    public InstallSnapshotResponse installSnapshot(InstallSnapshotRequest request) {
+        return raftService.installSnapshot(request);
+    }
+}
--- src/main/java/com/yundepot/raft/RaftClient.java
+++ src/main/java/com/yundepot/raft/RaftClient.java
@@ -1,32 +1,120 @@
 package com.yundepot.raft;
 
-import com.yundepot.oaa.config.GenericOption;
-import com.yundepot.raft.service.RaftService;
+import com.yundepot.raft.bean.Cluster;
+import com.yundepot.raft.bean.Response;
+import com.yundepot.raft.bean.Server;
+import com.yundepot.raft.common.ResponseCode;
+import com.yundepot.raft.service.PairService;
+import com.yundepot.raft.service.RaftAdminService;
+import com.yundepot.raft.util.ClusterUtil;
 import com.yundepot.rpc.RpcClient;
-import lombok.Data;
 
 /**
+ * raft 对外客户端
  * @author zhaiyanan
- * @date 2019/6/15 17:30
+ * @date 2022/7/12  14:21
  */
-@Data
 public class RaftClient {
 
-    private final String host;
-    private final int port;
+    /**
+     * 当前连接的节点
+     */
+    private Server server;
     private RpcClient rpcClient;
-    private RaftService raftService;
+    private RaftAdminService adminService;
+    private PairService pairService;
 
-    public RaftClient(String host, int port) {
-        this.host = host;
-        this.port = port;
-        this.rpcClient = new RpcClient(host, port);
-        this.rpcClient.option(GenericOption.TCP_HEARTBEAT_SWITCH, false);
-        this.rpcClient.start();
-        raftService = this.rpcClient.create(RaftService.class);
+    public RaftClient(String cluster) {
+        this(ClusterUtil.parserCluster(cluster));
     }
 
-    public void shutdown() {
-        this.rpcClient.shutdown();
+    public RaftClient(Cluster cluster) {
+        this.server = cluster.getServers().stream().findAny().get();
+        this.rpcClient = new RpcClient(server.getHost(), server.getPort());
+        this.adminService = rpcClient.create(RaftAdminService.class);
+        this.pairService = rpcClient.create(PairService.class);
+    }
+
+    /**
+     * 写入数据
+     */
+    public void put(byte[] key, byte[] value) {
+        // todo 连接异常情况
+        Response response = pairService.put(key, value);
+        handleResponse(response);
+        // todo 优雅的重试方案
+        if (response.getCode() == ResponseCode.NOT_LEADER.getValue()) {
+            put(key, value);
+        }
+    }
+
+    /**
+     * 读取数据
+     * @param key
+     * @return
+     */
+    public byte[] get(byte[] key) {
+        Response response = pairService.get(key);
+        handleResponse(response);
+        return (byte[]) response.getData();
+    }
+
+    /**
+     * 获取leader
+     * @return
+     */
+    public Server getLeader() {
+        return adminService.getLeader();
+    }
+
+    /**
+     * 获取集群节点信息
+     * @return
+     */
+    public Cluster getClusterInfo() {
+        return adminService.getClusterInfo();
+    }
+
+    /**
+     * 添加节点
+     * @param server
+     * @return
+     */
+    public Response addPeer(Server server) {
+        Response response = adminService.addPeer(server);
+        handleResponse(response);
+        return response;
+    }
+
+    /**
+     * 移除节点
+     * @param server
+     * @return
+     */
+    public Response removePeer(Server server) {
+        Response response = adminService.removePeer(server);
+        // todo 重试
+        handleResponse(response);
+        return response;
+    }
+
+    /**
+     * 获取节点的日志提交索引
+     * @return
+     */
+    public long getCommitIndex() {
+        return adminService.getCommitIndex();
+    }
+
+    private void handleResponse(Response response) {
+        // 重定向到leader节点
+        if (ResponseCode.NOT_LEADER.getValue() == response.getCode()) {
+            Server leader = (Server) response.getData();
+            this.server = leader;
+            rpcClient.shutdown();
+            rpcClient = new RpcClient(server.getHost(), server.getPort());
+            this.adminService = rpcClient.create(RaftAdminService.class);
+            this.pairService = rpcClient.create(PairService.class);
+        }
     }
 }
