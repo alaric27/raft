2e5751e9736f5b074e8cd2a4f6a3ed68630b6987
zhaiyanan <429276924@qq.com>
2022-07-14 17:32:03 +0800

    prevote

--- src/main/java/com/yundepot/raft/common/Constant.java
+++ src/main/java/com/yundepot/raft/common/Constant.java
@@ -1,7 +1,5 @@
 package com.yundepot.raft.common;
 
-import java.nio.charset.StandardCharsets;
-
 /**
  * @author zhaiyanan
  * @date 2022/6/22  10:37
@@ -12,6 +10,4 @@ public class Constant {
      * 代表没有leader
      */
     public static final int NO_LEADER = 0;
-
-    public static final byte[] METADATA = "metadata".getBytes(StandardCharsets.UTF_8);
 }
--- src/test/java/com/yundepot/raft/RocksDBTest.java
+++ src/test/java/com/yundepot/raft/RocksDBTest.java
@@ -1,8 +1,5 @@
 package com.yundepot.raft;
 
-import com.alibaba.fastjson.JSON;
-import com.yundepot.raft.bean.SnapshotMetadata;
-import com.yundepot.raft.common.Constant;
 import org.rocksdb.*;
 
 import java.nio.charset.StandardCharsets;
@@ -16,28 +13,27 @@ import java.util.List;
 public class RocksDBTest {
     private static String dataDir = "/Users/zyn/Desktop/candelete/node1/data/";
 
-    public static void main(String[] args) throws Exception {
+    static {
         RocksDB.loadLibrary();
+    }
 
+    public static void main(String[] args) throws Exception {
         DBOptions options = new DBOptions();
         options.setCreateIfMissing(true);
         options.setCreateMissingColumnFamilies(true);
-
         List<ColumnFamilyDescriptor> descriptorList = new ArrayList<>();
         descriptorList.add(new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY));
-        descriptorList.add(new ColumnFamilyDescriptor("config".getBytes(StandardCharsets.UTF_8)));
 
         List<ColumnFamilyHandle> handleList = new ArrayList<>();
         RocksDB rocksDB = RocksDB.openReadOnly(options, dataDir, descriptorList, handleList);
-        assert (handleList.size() == 2);
+        assert (handleList.size() == 1);
         ColumnFamilyHandle defaultHandle = handleList.get(0);
-        ColumnFamilyHandle configHandle = handleList.get(1);
-        System.out.println(rocksDB.getColumnFamilyMetaData(defaultHandle).size() / 1024 /1024);
 
-        byte[] bytes = rocksDB.get(configHandle, Constant.METADATA);
-        if (bytes != null) {
-            SnapshotMetadata metadata = JSON.parseObject(bytes, SnapshotMetadata.class);
-            System.out.println(JSON.toJSONString(metadata));
+        RocksIterator it = rocksDB.newIterator(defaultHandle);
+        for (it.seekToFirst(); it.isValid(); it.next()) {
+            byte[] key = it.key();
+            byte[] value = it.value();
+            System.out.println();
         }
     }
 }
