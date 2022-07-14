f67dd2d9d934a6d646f9f1be6d8c498284f5449f
zhaiyanan <429276924@qq.com>
2022-07-15 06:56:32 +0800

    RocksDBTest

--- src/test/java/com/yundepot/raft/RocksDBTest.java
+++ src/test/java/com/yundepot/raft/RocksDBTest.java
@@ -1,5 +1,7 @@
 package com.yundepot.raft;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.rocksdb.*;
 
 import java.nio.charset.StandardCharsets;
@@ -11,29 +13,44 @@ import java.util.List;
  * @date 2022/6/30  10:45
  */
 public class RocksDBTest {
-    private static String dataDir = "/Users/zyn/Desktop/candelete/node1/data/";
 
     static {
         RocksDB.loadLibrary();
     }
+    private RocksDB rocksDB;
+    private ColumnFamilyHandle defaultHandle;
 
-    public static void main(String[] args) throws Exception {
+    @Before
+    public void before() throws Exception {
         DBOptions options = new DBOptions();
         options.setCreateIfMissing(true);
         options.setCreateMissingColumnFamilies(true);
         List<ColumnFamilyDescriptor> descriptorList = new ArrayList<>();
         descriptorList.add(new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY));
 
+        String dataDir = "/Users/zyn/Desktop/candelete/node1/data/";
         List<ColumnFamilyHandle> handleList = new ArrayList<>();
-        RocksDB rocksDB = RocksDB.openReadOnly(options, dataDir, descriptorList, handleList);
+        rocksDB = RocksDB.openReadOnly(options, dataDir, descriptorList, handleList);
         assert (handleList.size() == 1);
-        ColumnFamilyHandle defaultHandle = handleList.get(0);
+        defaultHandle = handleList.get(0);
+    }
 
+    @Test
+    public void scan() {
         RocksIterator it = rocksDB.newIterator(defaultHandle);
         for (it.seekToFirst(); it.isValid(); it.next()) {
             byte[] key = it.key();
             byte[] value = it.value();
             System.out.println();
+        }
+    }
+
+    @Test
+    public void put() throws Exception {
+        for (long i = 1; i < 1000; i++) {
+            byte[] key = ("aaa" + i).getBytes(StandardCharsets.UTF_8);
+            byte[] value = RaftClientTest.getRandomString(10).getBytes(StandardCharsets.UTF_8);
+            rocksDB.put(defaultHandle, key, value);
         }
     }
 }
